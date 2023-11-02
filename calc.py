b90caf203602474cf9a82f4605d0fab2343893b9
Thomas <65510993+tc3149@users.noreply.github.com>
2023-11-03 03:33:29 +1100

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,23 @@
+# adds two numbers
+def add(x, y):
+    return x + y
+
+# subtracts two numbers
+def subtract(x, y):
+    return x - y
+
+# multiplies two numbers
+def multiply(x, y):
+    return x * y
+
+# divides two numbers
+def divide(x, y):
+    return x / y 
+
+# returns the modulus of two numbers
+def mod(x, y):
+    return x % y
+
+# returns the exponent x^y
+def exp(x, y):
+    return x ** y
